cf11ef8e0c49a0e1513c88b770b2a48b40c9d10e
Soluevo Mac Mini 04 <mm04@Mac-mini-2.local>
2022-08-17 17:30:40 -0300

    Typescript Projeto Day 5.1

--- typescript-curso-1-arquivos-iniciais/app/views/negociacoes-view.ts
+++ typescript-curso-1-arquivos-iniciais/app/views/negociacoes-view.ts
@@ -20,7 +20,8 @@ export class NegociacoesView extends View<Negociacoes> {
                             <td>${this.formatar(negociacao.data)}</td>
                             <td>${negociacao.quantidade}</td>
                             <td>${negociacao.valor}</td>
-                        </tr>`
+                        </tr>
+                        `
                     }).join('')}
                 </tbody>
             </table>
